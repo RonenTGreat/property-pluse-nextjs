40cc95aa62155e7c2285617fcac10788bb8f8350
RonenTGreat <ronenh53@gmail.com>
2026-08-11 12:42:28 +0000

    created base pages and added nav markup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,21 @@
-import '@/assets/styles/globals.css'
+import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
-    title: 'Property Pulse',
-    keywords: 'rental, property, real estate',
-    description: 'Find the perfect rental property'
-}
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
 
-const MainLayout = ({children}) => {
-    return ( 
-        <html>
-            <body>
-                <main>
-                    {children}
-                </main>
-            </body>
-        </html>
-     );
-}
- 
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
 export default MainLayout;
