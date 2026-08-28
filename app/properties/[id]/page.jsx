c862dc70b803c0136187f10de6371930090b6ca9
RonenTGreat <ronenh53@gmail.com>
2026-08-28 13:02:48 +0000

    .

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,6 +4,7 @@ import Property from "@/models/Property";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
+import PropertyImages from "@/components/PropertyImages";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -30,6 +31,7 @@ const PropertyPage = async ({ params }) => {
           </div>
         </div>
       </section>
+      <PropertyImages images={property.images} />
     </>
   );
 };
