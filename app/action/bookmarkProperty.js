0a7066e6f150c06cc66897b6a305ad480e7487f7
RonenTGreat <ronenh53@gmail.com>
2026-09-07 15:21:37 +0000

    .

---
+++ app/action/bookmarkProperty.js
@@ -0,0 +1,44 @@
+"use server";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+
+async function bookmarkProperty(propertyId) {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+  const { userId } = sessionUser;
+
+  const user = await User.findById(userId);
+
+  const isBookmarked = user.bookmarks.inlcudes(propertyId);
+
+  let message;
+
+  if (isBookmarked) {
+    // if already bookmarked then remove
+    user.bookmarks.pull(propertyId);
+    message = "Bookmark Removed";
+    isBookmarked = false;
+  } else {
+    // If not bookmarked, then add
+    user.bookmarks.pull(propertyId);
+    message = "Bookmark Added";
+    isBookmarked = true;
+  }
+
+  await user.save();
+  revalidatePath('/properties/saved', 'page')
+
+  return {
+    message,
+    isBookmarked
+  }
+}
+
+export default bookmarkProperty;
--- app/properties/[id]/edit/page.jsx
+++ app/properties/[id]/edit/page.jsx
@@ -2,7 +2,7 @@ import PropertyEditForm from "@/components/PropertyEditForm";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { convertToSerializableObject } from "@/utils/convertToObject";
-import { CgEnter } from "react-icons/cg";
+
 
 const PropertyEditPage = async ({ params }) => {
   await connectDB();
@@ -21,6 +21,7 @@ const PropertyEditPage = async ({ params }) => {
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <PropertyEditForm property={property} />
+          
         </div>
       </div>
     </section>
